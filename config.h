c42b86bb20e8ba32b62e19e6b4580b3726ba0041
22maria-anton <maria.anton@student.upt.ro>
2026-05-13 12:24:04 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,12 +1,25 @@
-config.h
-types.h
-game.h
-  game.c
-graphics.h
-  graphics.c
-ui.h
-  ui.c
-score.h
-  score.c
-main.c 
-makefile
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Dimensiuni fereastră */
+#define SW          480      // screen width latime fereastra            
+#define SH          720     // inaltime fereastra           
+
+/* Fizică și mecanici */
+#define GRAVITY     900.0f //cu cat cade pasarea pe secund
+#define FLAP       -380.0f   // impuls la batai din aripi    
+
+/* Țevi și obstacole */
+#define PIPE_W       65      // latimea tevii                
+#define GAP         175      // spatiul dintre tevi          
+#define MAX_PIPES     6
+
+/* Teren și pasare */
+#define GROUND      100      // inaltimea solului            
+#define BIRD_R       22      // raza pasarela                
+#define N_SKINS       8
+
+/* Fișier înregistrări */
+#define HIGH_SCORE_FILE "hs.dat"
+
+#endif
